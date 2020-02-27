6bc4e50d1cc829b1ac05fdd77474a63a74330983
Julie Antonio <julieantonio38@gmail.com>
2020-02-26 21:54:18 -0500

    oops

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,40 @@
 
 import sys
 
+"""OP CODES"""
+LDI = 0b10000010
+PRN = 0b01000111
+MUL = 0b10100010
+ADD = 0b10100000
+SUB = 0b10100001
+HLT = 0b00000001
+PSH = 0b01000101
+POP = 0b01000110
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+        self.halt = False
+        self.sp = 7
+
+        # self.SP = self.reg[7]
+
+        self.branchtable = {
+            LDI: self.LDI,
+            PRN: self.PRN,
+            MUL: self.MUL,
+            ADD: self.ADD,
+            SUB: self.SUB,
+            HLT: self.HLT,
+            PSH: self.PSH,
+            POP: self.POP
+        }
+        
 
     def load(self):
         """Load a program into memory."""
@@ -16,29 +44,103 @@ class CPU:
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        program = []
 
+        if len(sys.argv) != 2:
+            print('Error')
+            sys.exit()
 
-    def alu(self, op, reg_a, reg_b):
+        with open(sys.argv[1]) as program:
+            for instruction in program:
+                seperate_comment = instruction.split('#') #separate out comments if any
+                instruction = seperate_comment[0].strip() #remove white space from what's left
+                if instruction == '': #if it's a comment or a blank line, ignore and keep going
+                    continue
+                int_value = int(instruction, 2) #convert binary string to integer
+
+                self.ram[address] = int_value
+                address += 1
+
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+
+    def ram_write(self, MDR, MAR):
+        self.ram[MAR] = MDR
+
+    def MUL(self):
+        self.alu('MUL')
+        self.pc += 3
+
+    def ADD(self):
+        self.alu('ADD')
+        self.pc +=3
+
+    def SUB(self):
+        self.alu('SUB')
+        self.pc += 3
+
+    def LDI(self):
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
+        self.reg[operand_a] = operand_b
+        self.pc += 3
+
+    def PRN(self):
+        operand_a = self.ram_read(self.pc + 1)
+        print(self.reg[operand_a])
+        self.pc += 2
+
+    def HLT(self):
+        self.halt = True
+
+    def alu(self, op):
         """ALU operations."""
+        reg_a = self.ram_read(self.pc + 1)
+        reg_b = self.ram_read(self.pc + 2)
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+
+        elif op == "SUB":
+            self.reg[reg_a] -= self.reg[reg_b]
+
+        elif op == 'MUL':
+            self.reg[reg_a] *= self.reg[reg_b]
+        
+        elif op == 'DIV':
+            if self.reg[reg_b] == 0:
+                print('error- dividing by 0')
+                self.halt = True
+            else:
+                self.reg[reg_a] /= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
+
+    def PSH(self):
+        self.reg[self.sp] -= 1
+        reg_num = self.ram[self.pc + 1]
+        reg_val = self.reg[reg_num]
+        self.ram[self.reg[self.sp]] = reg_val
+
+        self.pc += 2
+
+    def POP(self):
+        val = self.ram[self.reg[self.sp]]
+        reg_num = self.ram[self.pc + 1]
+        self.reg[reg_num] = val
+        self.reg[self.sp] += 1
+  
+        self.pc += 2
 
     def trace(self):
         """
@@ -62,4 +164,30 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        while self.halt == False:
+            IR = self.ram[self.pc]
+            # operand_a = self.ram_read(self.pc + 1)
+            # operand_b = self.ram_read(self.pc + 2)
+
+            # if IR ==  0b10000010: #LDI
+            #     self.reg[operand_a] = operand_b
+            #     self.pc += 3
+
+            # elif IR == 0b01000111: # PRN
+            #     print(self.reg[operand_a])
+            #     self.pc += 2
+
+            # elif IR == 0b10100010: #MUL
+            #     mul = self.reg[operand_a] * self.reg[operand_b]
+            #     self.reg[operand_a] = mul
+            #     self.pc += 3
+
+            # if IR == 0b00000001: #HLT
+            #     self.halt= True
+
+            if IR in self.branchtable:
+                self.branchtable[IR]()
+
+            else:
+                print('Unknown instruction')
+                sys.exit()
